deeb4828633741297c19205fedee041967eb8836
FikriSyafwan <ifhamsyafwan@gmail.com>
2024-09-05 12:17:02 +0700

    Update Problem1.cpp

--- Problem1.cpp
+++ Problem1.cpp
@@ -8,7 +8,7 @@ void generatePayslip(string name, double baseSalary, double percentage, double i
     double grossSalary = baseSalary * (percentage / 100.0);
 
     // Calculate Tax
-    double tax = grossSalary * 0.90;
+    double tax = grossSalary * 0.20;
 
     // Calculate Net Salary
     double netSalary = grossSalary - tax - installment - insurance;
@@ -19,18 +19,18 @@ void generatePayslip(string name, double baseSalary, double percentage, double i
     cout << "---------------------" << endl;
     cout << "Name: " << name << endl;
     cout << "Gross Salary: Rp" << grossSalary << endl;
-    cout << "Tax (90%): Rp" << tax << endl;
+    cout << "Tax (20%): Rp" << tax << endl;
     cout << "Installment: Rp" << installment << endl;
     cout << "Insurance: Rp" << insurance << endl;
     cout << "Net Salary: Rp" << netSalary << endl;
 }
 
 int main() {
-    string name = "Mas Agus";
-    double baseSalary = 250000000.00;  // Base salary
-    double percentage = 150;  // 150% of the base salary
-    double installment = 2000000.00;
-    double insurance = 1500000.00;
+    string name = "John Doe";
+    double baseSalary = 15000000.00;  // Base salary
+    double percentage = 100;  // 100% of the base salary
+    double installment = 200000.00;
+    double insurance = 150000.00;
 
     generatePayslip(name, baseSalary, percentage, installment, insurance);
 
